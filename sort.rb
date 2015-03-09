00c20b722a7c956ed8f892bcdeb82724900d7b92
Sebastian Ludwig <sebastian@lurado.de>
2015-03-09 15:32:28 +0700

    Modified implementation to follow the notation of paper as close as possible (except for the 0 based indices).

--- sort.rb
+++ sort.rb
@@ -3,105 +3,114 @@ require 'matrix'
 # class Element
 #     attr_accessor :name, :index
 # end
-def check_tournament(t)
-    t.each_with_index(:upper) do |e, row, col|
-        raise "#{row}, #{col} must be 1, 0 or nil and is #{e}" unless [0, 1, nil].include? e
-        if row == col
-            raise "#{row}, #{col} ought to be nil" unless e.nil?
-        else
-            raise "mismatch in #{row}, #{col} and #{col}, #{row}" if t[col,row] == e
+
+class SemiHeapSort
+    @@tournament = nil
+    def self.tournament
+        @@tournament
+    end
+    def self.tournament=(t)
+        self.check_tournament t
+        @@tournament = t
+    end
+
+    def self.check_tournament(t)
+        t.each_with_index(:upper) do |e, row, col|
+            raise "#{row}, #{col} must be 1, 0 or nil and is #{e}" unless [0, 1, nil].include? e
+            if row == col
+                raise "#{row}, #{col} ought to be nil" unless e.nil?
+            else
+                raise "mismatch in #{row}, #{col} and #{col}, #{row}" if t[col,row] == e
+            end
         end
     end
-end
 
-elements = (0..7).to_a
-
-n = nil
-T = Matrix[         # Tournament
-    [n, 0, 1, 0, 1, 0, 1, 1],
-    [1, n, 0, 1, 0, 1, 0, 1],
-    [0, 1, n, 0, 0, 1, 0, 0],
-    [1, 0, 1, n, 1, 1, 0, 1],
-    [0, 1, 1, 0, n, 1, 1, 1],
-    [1, 0, 0, 0, 0, n, 0, 0],
-    [0, 1, 1, 1, 0, 1, n, 0],
-    [0, 0, 1, 0, 0, 1, 1, n]
-]
-check_tournament T
-
-# TODO: get rid of tournament parameter here and use T or objects i and j -> get rid of all tournament parameters
-def beats?(tournament, i, j)
-    return false if i.nil?
-    return true if j.nil?
-    return j >= tournament.column_size || tournament[i, j] == 1
-end
-
-def max(tournament, i, j, k)
-    return i if beats?(tournament, i, j) && beats?(tournament, i, k)
-    return j if beats?(tournament, j, i) && beats?(tournament, j, k)
-    return k if beats?(tournament, k, i) && beats?(tournament, k, j)
-    return nil
-end
-
-def is_max?(tournament, i, j, k)     # if i beats j and k
-    max(tournament, i, j, k) == i
-end
-
-def is_semi_max?(tournament, i, j, k)    # if i is semi max, if neither j nor k are max
-    !is_max?(tournament, j, i, k) && !is_max?(tournament, k, i, j)
-end
-
-def left(i)
-    2 * i + 1
-end
-
-def right(i)
-    2 * i + 2
-end
-
-def semi_heapify(a, i)
-    if not is_semi_max?(T, i, left(i), right(i))
-        winner = max(T, i, left(i), right(i))
-        raise "no winner" if winner.nil?
-        a[i], a[winner] = a[winner], a[i]
-        semi_heapify(a, winner)
+    # TODO: get rid of tournament parameter here and use T or objects i and j -> get rid of all tournament parameters
+    def self.beats?(i, j)
+        return false if i.nil?
+        return true if j.nil?
+        return j >= @@tournament.column_size || @@tournament[i, j] == 1
     end
-end
 
-def build_semi_heap(a)
-    ((a.size - 1) / 4).floor.downto(0) do |i|
-        semi_heapify(a, i)
+    def self.max(i, j, k)
+        return i if beats?(i, j) && beats?(i, k)
+        return j if beats?(j, i) && beats?(j, k)
+        return k if beats?(k, i) && beats?(k, j)
+        return nil
     end
-end
 
-def replace(tournament, a, i)
-    if a[left(i)].nil? && a[right(i)].nil?
-        a[i] = nil
-    elsif beats?(tournament, a[i], a[left(i)]) && beats?(tournament, a[left(i)], a[right(i)])
-        a[i] = a[left(i)]
-        replace(tournament, a, left(i))
-    else
-        a[i] = a[right(i)]
-        replace(tournament, a, right(i))
+    def self.is_max?(i, j, k)     # if i beats j and k
+        max(i, j, k) == i
     end
-end
 
-def semi_heap_sort(tournament, elements)
-    a = elements + [nil] * elements.size
+    def self.is_semi_max?(i, j, k)    # if i is semi max, if neither j nor k are max
+        !is_max?(j, i, k) && !is_max?(k, i, j)
+    end
 
-    build_semi_heap(a)
+    def self.left(i)
+        2 * i + 1
+    end
 
-    # puts a.inspect
+    def self.right(i)
+        2 * i + 2
+    end
 
-    result = []
-    while a[left(0)] != nil || a[right(0)] != nil
+    def self.replace(a, i)
+        if a[left(i)].nil? && a[right(i)].nil?
+            a[i] = nil
+        elsif beats?(a[i], a[left(i)]) && beats?(a[left(i)], a[right(i)])
+            a[i] = a[left(i)]
+            replace(a, left(i))
+        else
+            a[i] = a[right(i)]
+            replace(a, right(i))
+        end
+    end
+
+    def self.semi_heapify(a, i)
+        if not is_semi_max?(i, left(i), right(i))
+            winner = max(i, left(i), right(i))
+            raise "no winner" if winner.nil?
+            a[i], a[winner] = a[winner], a[i]
+            semi_heapify(a, winner)
+        end
+    end
+
+    def self.build_semi_heap(a)
+        ((a.size - 1) / 4).floor.downto(0) do |i|
+            semi_heapify(a, i)
+        end
+    end
+
+    def self.semi_heap_sort(elements)
+        a = elements + [nil] * elements.size
+
+        build_semi_heap(a)
+
+        result = []
+        while a[left(0)] != nil || a[right(0)] != nil
+            result << a[0]
+            replace(a, 0)
+        end
         result << a[0]
-        replace(tournament, a, 0)
     end
-    result << a[0]
 end
+
 
 if __FILE__ == $0
-    puts semi_heap_sort(T, elements).inspect
+    n = nil
+    T = Matrix[         # Tournament
+        [n, 0, 1, 0, 1, 0, 1, 1],
+        [1, n, 0, 1, 0, 1, 0, 1],
+        [0, 1, n, 0, 0, 1, 0, 0],
+        [1, 0, 1, n, 1, 1, 0, 1],
+        [0, 1, 1, 0, n, 1, 1, 1],
+        [1, 0, 0, 0, 0, n, 0, 0],
+        [0, 1, 1, 1, 0, 1, n, 0],
+        [0, 0, 1, 0, 0, 1, 1, n]
+    ]
+    SemiHeapSort.check_tournament T
+    SemiHeapSort.tournament = T
+    puts SemiHeapSort.semi_heap_sort((0..7).to_a).inspect
 end
 
