67a650577d3972af7a2c2a1c57121543f43d6074
Sebastian Ludwig <sebastian@lurado.de>
2015-04-01 20:08:55 +0700

    Using passed block to compare elements instead of class variable @@tournament.

--- sort.rb
+++ sort.rb
@@ -1,19 +1,6 @@
 require 'matrix'
 
-# class Element
-#     attr_accessor :name, :index
-# end
-
 class SemiHeapSort
-    @@tournament = nil
-    def self.tournament
-        @@tournament
-    end
-    def self.tournament=(t)
-        self.check_tournament t
-        @@tournament = t
-    end
-
     def self.check_tournament(t)
         t.each_with_index(:upper) do |e, row, col|
             raise "#{row}, #{col} must be 1, 0 or nil and is #{e}" unless [0, 1, nil].include? e
@@ -25,26 +12,27 @@ class SemiHeapSort
         end
     end
 
-    # TODO: get rid of tournament parameter here and use T or objects i and j -> get rid of all tournament parameters
-    def self.beats?(a_i, a_j)
+    def self.beats?(a_i, a_j, &comparator)
         return false if a_i.nil?
         return true if a_j.nil?
-        return a_j[:index] >= @@tournament.column_size || @@tournament[a_i[:index], a_j[:index]] == 1
+        result = comparator.call(a_i, a_j)
+        raise "invalid comparison result #{result} - must be -1 or 1" if result != 1 and result != -1
+        return result == -1
     end
 
-    def self.max(a_i, a_j, a_k)
-        return a_i if beats?(a_i, a_j) && beats?(a_i, a_k)
-        return a_j if beats?(a_j, a_i) && beats?(a_j, a_k)
-        return a_k if beats?(a_k, a_i) && beats?(a_k, a_j)
+    def self.max(a_i, a_j, a_k, &comparator)
+        return a_i if beats?(a_i, a_j, &comparator) && beats?(a_i, a_k, &comparator)
+        return a_j if beats?(a_j, a_i, &comparator) && beats?(a_j, a_k, &comparator)
+        return a_k if beats?(a_k, a_i, &comparator) && beats?(a_k, a_j, &comparator)
         return nil
     end
 
-    def self.is_max?(a_i, a_j, a_k)     # if i beats j and k
-        max(a_i, a_j, a_k) == a_i
+    def self.is_max?(a_i, a_j, a_k, &comparator)     # if i beats j and k
+        max(a_i, a_j, a_k, &comparator) == a_i
     end
 
-    def self.is_semi_max?(a_i, a_j, a_k)    # if i is semi max, if neither j nor k are max
-        !is_max?(a_j, a_i, a_k) && !is_max?(a_k, a_i, a_j)
+    def self.is_semi_max?(a_i, a_j, a_k, &comparator)    # if i is semi max, if neither j nor k are max
+        !is_max?(a_j, a_i, a_k, &comparator) && !is_max?(a_k, a_i, a_j, &comparator)
     end
 
     def self.left(index)
@@ -55,43 +43,44 @@ class SemiHeapSort
         2 * index + 2
     end
 
-    def self.replace(a, i)
+    def self.replace(a, i, &comparator)
         if a[left(i)].nil? && a[right(i)].nil?
             a[i] = nil
-        elsif beats?(a[i], a[left(i)]) && beats?(a[left(i)], a[right(i)])
+        elsif beats?(a[i], a[left(i)], &comparator) && beats?(a[left(i)], a[right(i)], &comparator)
             a[i] = a[left(i)]
-            replace(a, left(i))
+            replace(a, left(i), &comparator)
         else
             a[i] = a[right(i)]
-            replace(a, right(i))
+            replace(a, right(i), &comparator)
         end
     end
 
-    def self.semi_heapify(a, i)
-        if not is_semi_max?(a[i], a[left(i)], a[right(i)])
-            winner = max(a[i], a[left(i)], a[right(i)])
+    def self.semi_heapify(a, i, &comparator)
+        if not is_semi_max?(a[i], a[left(i)], a[right(i)], &comparator)
+            winner = max(a[i], a[left(i)], a[right(i)], &comparator)
             raise "no winner" if winner.nil?
             winner_index = winner == a[left(i)] ? left(i) : right(i)
             a[i], a[winner_index] = a[winner_index], a[i]
-            semi_heapify(a, winner_index)
+            semi_heapify(a, winner_index, &comparator)
         end
     end
 
-    def self.build_semi_heap(a)
+    def self.build_semi_heap(a, &comparator)
         ((a.size - 1) / 4).floor.downto(0) do |i|
-            semi_heapify(a, i)
+            semi_heapify(a, i, &comparator)
         end
     end
 
-    def self.semi_heap_sort(elements)
+    def self.semi_heap_sort(elements, &comparator)
+        raise "no comparison block given" unless block_given?
         a = elements + [nil] * elements.size
 
-        build_semi_heap(a)
+        build_semi_heap(a, &comparator)
 
         result = []
         while a[left(0)] != nil || a[right(0)] != nil
             result << a[0]
-            replace(a, 0)
+            replace(a, 0, &comparator)
         end
         result << a[0]
     end
@@ -111,12 +100,11 @@ if __FILE__ == $0
         [0, 0, 1, 0, 0, 1, 1, n]
     ]
     SemiHeapSort.check_tournament T
-    SemiHeapSort.tournament = T
     # puts SemiHeapSort.semi_heap_sort((0..7).to_a).inspect
 
     elements = (0..7).map { |i| {index: i, label: "n#{i + 1}"} }
-    elements.reverse!
-    SemiHeapSort.semi_heap_sort(elements).each { |e| print e[:label], " " }
+    result = SemiHeapSort.semi_heap_sort(elements) { |a, b| T[a[:index], b[:index]] == 1 ? -1 : 1 }
+    result.each { |e| print e[:label], " " }
     puts
 end
 
--- spec/semi_heap_spec.rb
+++ spec/semi_heap_spec.rb
@@ -1,10 +1,14 @@
 require_relative '../sort'
 
 describe SemiHeapSort do
+    before do
+        @comp = Proc.new { |a, b| @tournament[a, b] == 1 ? -1 : 1 }
+    end
+
     context 'Figure 2 (a)' do
         before do
             n = nil
-            SemiHeapSort.tournament = Matrix[
+            @tournament = Matrix[
                 [n, 1, 1],
                 [0, n, 1],
                 [0, 0, n]
@@ -12,30 +16,30 @@ describe SemiHeapSort do
         end
 
         describe 'max' do
-            it 'finds 0 as max' do expect(SemiHeapSort.max(0, 1, 2)).to eq 0 end
+            it 'finds 0 as max' do expect(SemiHeapSort.max(0, 1, 2, &@comp)).to eq 0 end
 
             it 'finds 0 as max regardless of the arguemnt order' do
-                expect(SemiHeapSort.max(0, 2, 1)).to eq 0
-                expect(SemiHeapSort.max(1, 0, 2)).to eq 0
-                expect(SemiHeapSort.max(2, 0, 1)).to eq 0
-                expect(SemiHeapSort.max(1, 2, 0)).to eq 0
-                expect(SemiHeapSort.max(2, 1, 0)).to eq 0
+                expect(SemiHeapSort.max(0, 2, 1, &@comp)).to eq 0
+                expect(SemiHeapSort.max(1, 0, 2, &@comp)).to eq 0
+                expect(SemiHeapSort.max(2, 0, 1, &@comp)).to eq 0
+                expect(SemiHeapSort.max(1, 2, 0, &@comp)).to eq 0
+                expect(SemiHeapSort.max(2, 1, 0, &@comp)).to eq 0
             end
 
-            it 'reports 0 as max' do expect(SemiHeapSort.is_max?(0, 1, 2)).to eq true end
-            it 'reports 1 as NOT max' do expect(SemiHeapSort.is_max?(1, 0, 2)).to eq false end
-            it 'reports 2 as NOT max' do expect(SemiHeapSort.is_max?(2, 0, 1)).to eq false end
+            it 'reports 0 as max' do expect(SemiHeapSort.is_max?(0, 1, 2, &@comp)).to eq true end
+            it 'reports 1 as NOT max' do expect(SemiHeapSort.is_max?(1, 0, 2, &@comp)).to eq false end
+            it 'reports 2 as NOT max' do expect(SemiHeapSort.is_max?(2, 0, 1, &@comp)).to eq false end
         end
 
         describe 'semi max' do
-            it 'reports 0 as semi max' do expect(SemiHeapSort.is_semi_max?(0, 1, 2)).to eq  true end
-            it 'reports 1 as NOT semi max' do expect(SemiHeapSort.is_semi_max?(1, 0, 2)).to eq  false end
-            it 'reports 2 as NOT semi max' do expect(SemiHeapSort.is_semi_max?(2, 0, 1)).to eq  false end
+            it 'reports 0 as semi max' do expect(SemiHeapSort.is_semi_max?(0, 1, 2, &@comp)).to eq  true end
+            it 'reports 1 as NOT semi max' do expect(SemiHeapSort.is_semi_max?(1, 0, 2, &@comp)).to eq  false end
+            it 'reports 2 as NOT semi max' do expect(SemiHeapSort.is_semi_max?(2, 0, 1, &@comp)).to eq  false end
         end
 
         describe 'semi heap sort' do
             it 'sorts items correctly' do
-                expect(SemiHeapSort.semi_heap_sort((0..2).to_a)).to eq [0, 1, 2]
+                expect(SemiHeapSort.semi_heap_sort((0..2).to_a, &@comp)).to eq [0, 1, 2]
             end
         end
     end
@@ -43,7 +47,7 @@ describe SemiHeapSort do
     context 'Figure 2 (b)' do
         before do
             n = nil
-            SemiHeapSort.tournament = Matrix[
+            @tournament = Matrix[
                 [n, 1, 1],
                 [0, n, 0],
                 [0, 1, n]
@@ -51,18 +55,18 @@ describe SemiHeapSort do
         end
 
         describe 'max' do
-            it 'finds 0 as max' do expect(SemiHeapSort.max(0, 1, 2)).to eq 0 end
+            it 'finds 0 as max' do expect(SemiHeapSort.max(0, 1, 2, &@comp)).to eq 0 end
         end
 
         describe 'semi max' do
-            it 'reports 0 as semi max' do expect(SemiHeapSort.is_semi_max?(0, 1, 2)).to eq  true end
-            it 'reports 1 as NOT semi max' do expect(SemiHeapSort.is_semi_max?(1, 0, 2)).to eq  false end
-            it 'reports 2 as NOT semi max' do expect(SemiHeapSort.is_semi_max?(2, 0, 1)).to eq  false end
+            it 'reports 0 as semi max' do expect(SemiHeapSort.is_semi_max?(0, 1, 2, &@comp)).to eq  true end
+            it 'reports 1 as NOT semi max' do expect(SemiHeapSort.is_semi_max?(1, 0, 2, &@comp)).to eq  false end
+            it 'reports 2 as NOT semi max' do expect(SemiHeapSort.is_semi_max?(2, 0, 1, &@comp)).to eq  false end
         end
 
         describe 'semi heap sort' do
             it 'sorts items correctly' do
-                expect(SemiHeapSort.semi_heap_sort((0..2).to_a)).to eq [0, 2, 1]
+                expect(SemiHeapSort.semi_heap_sort((0..2).to_a, &@comp)).to eq [0, 2, 1]
             end
         end
     end
@@ -70,7 +74,7 @@ describe SemiHeapSort do
     context 'Figure 2 (c)' do
         before do
             n = nil
-            SemiHeapSort.tournament = Matrix[
+            @tournament = Matrix[
                 [n, 1, 0],
                 [0, n, 1],
                 [1, 0, n]
@@ -78,18 +82,18 @@ describe SemiHeapSort do
         end
 
         describe 'max' do
-            it 'finds no max' do expect(SemiHeapSort.max(0, 1, 2)).to eq nil end
+            it 'finds no max' do expect(SemiHeapSort.max(0, 1, 2, &@comp)).to eq nil end
         end
 
         describe 'semi max' do
-            it 'reports 0 as semi max' do expect(SemiHeapSort.is_semi_max?(0, 1, 2)).to eq  true end
-            it 'reports 1 as semi max' do expect(SemiHeapSort.is_semi_max?(1, 0, 2)).to eq  true end
-            it 'reports 2 as semi max' do expect(SemiHeapSort.is_semi_max?(2, 0, 1)).to eq  true end
+            it 'reports 0 as semi max' do expect(SemiHeapSort.is_semi_max?(0, 1, 2, &@comp)).to eq  true end
+            it 'reports 1 as semi max' do expect(SemiHeapSort.is_semi_max?(1, 0, 2, &@comp)).to eq  true end
+            it 'reports 2 as semi max' do expect(SemiHeapSort.is_semi_max?(2, 0, 1, &@comp)).to eq  true end
         end
 
         describe 'semi heap sort' do
             it 'sorts items correctly' do
-                expect(SemiHeapSort.semi_heap_sort((0..2).to_a)).to eq [0, 1, 2]
+                expect(SemiHeapSort.semi_heap_sort((0..2).to_a, &@comp)).to eq [0, 1, 2]
             end
         end
     end
@@ -97,7 +101,7 @@ describe SemiHeapSort do
     context 'Figure 2 (d)' do
         before do
             n = nil
-            SemiHeapSort.tournament = Matrix[
+            @tournament = Matrix[
                 [n, 0, 1],
                 [1, n, 0],
                 [0, 1, n]
@@ -106,25 +110,25 @@ describe SemiHeapSort do
 
         describe 'beats?' do
             it 'works' do 
-                expect(SemiHeapSort.beats?(0, 2)).to eq true
-                expect(SemiHeapSort.beats?(1, 0)).to eq true
-                expect(SemiHeapSort.beats?(2, 1)).to eq true
+                expect(SemiHeapSort.beats?(0, 2, &@comp)).to eq true
+                expect(SemiHeapSort.beats?(1, 0, &@comp)).to eq true
+                expect(SemiHeapSort.beats?(2, 1, &@comp)).to eq true
             end
         end
 
         describe 'max' do
-            it 'finds no max' do expect(SemiHeapSort.max(0, 1, 2)).to eq nil end
+            it 'finds no max' do expect(SemiHeapSort.max(0, 1, 2, &@comp)).to eq nil end
         end
 
         describe 'semi max' do
-            it 'reports 0 as semi max' do expect(SemiHeapSort.is_semi_max?(0, 1, 2)).to eq  true end
-            it 'reports 1 as semi max' do expect(SemiHeapSort.is_semi_max?(1, 0, 2)).to eq  true end
-            it 'reports 2 as semi max' do expect(SemiHeapSort.is_semi_max?(2, 0, 1)).to eq  true end
+            it 'reports 0 as semi max' do expect(SemiHeapSort.is_semi_max?(0, 1, 2, &@comp)).to eq  true end
+            it 'reports 1 as semi max' do expect(SemiHeapSort.is_semi_max?(1, 0, 2, &@comp)).to eq  true end
+            it 'reports 2 as semi max' do expect(SemiHeapSort.is_semi_max?(2, 0, 1, &@comp)).to eq  true end
         end
 
         describe 'semi heap sort' do
             it 'sorts items correctly' do
-                expect(SemiHeapSort.semi_heap_sort((0..2).to_a)).to eq [0, 2, 1]
+                expect(SemiHeapSort.semi_heap_sort((0..2).to_a, &@comp)).to eq [0, 2, 1]
             end
         end
     end
@@ -132,7 +136,7 @@ describe SemiHeapSort do
     context 'example tournament' do
         before do
             n = nil
-            SemiHeapSort.tournament = Matrix[
+            @tournament = Matrix[
                 [n, 0, 1, 0, 1, 0, 1, 1],
                 [1, n, 0, 1, 0, 1, 0, 1],
                 [0, 1, n, 0, 0, 1, 0, 0],
@@ -146,35 +150,44 @@ describe SemiHeapSort do
 
         describe 'beats?' do
             it 'works' do 
-                expect(SemiHeapSort.beats?(6, 2)).to eq true
-                expect(SemiHeapSort.beats?(2, 6)).to eq false
+                expect(SemiHeapSort.beats?(6, 2, &@comp)).to eq true
+                expect(SemiHeapSort.beats?(2, 6, &@comp)).to eq false
             end
         end
 
         describe 'max' do
-            it 'finds 3 as max in triangle 3, 6, 7' do expect(SemiHeapSort.max(2, 5, 6)).to eq 6 end
-            it 'finds 4 as max in triangle 4, 8' do expect(SemiHeapSort.max(4, 8, 9)).to eq 4 end
+            it 'finds 7 as max in triangle 3, 6, 7' do expect(SemiHeapSort.max(2, 5, 6, &@comp)).to eq 6 end
+            it 'finds 4 as max in triangle 4, 8' do expect(SemiHeapSort.max(3, 7, nil, &@comp)).to eq 3 end
 
-            it 'finds no max in 2, 4, 5' do expect(SemiHeapSort.max(1, 3, 4)).to eq nil end
-            it 'finds no max in 1, 2, 3' do expect(SemiHeapSort.max(0, 1, 2)).to eq nil end
+            it 'finds no max in 2, 4, 5' do expect(SemiHeapSort.max(1, 3, 4, &@comp)).to eq nil end
+            it 'finds no max in 1, 2, 3' do expect(SemiHeapSort.max(0, 1, 2, &@comp)).to eq nil end
+        end
+
+        describe 'is_max?' do
+            it 'reports 4 as max in 4, 8' do expect(SemiHeapSort.is_max?(3, 7, nil, &@comp)).to eq true end
+            it 'reports 8 as NOT max in 4, 8' do expect(SemiHeapSort.is_max?(7, 3, nil, &@comp)).to eq false end
+            it 'reports nil as NOT max in 4, 8' do expect(SemiHeapSort.is_max?(nil, 3, 7, &@comp)).to eq false end
         end
 
         describe 'semi max' do
-            it 'reports 7 as semi max in 3, 6, 7' do expect(SemiHeapSort.is_semi_max?(6, 2, 5)).to eq true end
-            it 'reports 3 as NOT semi max in 3, 6, 7' do expect(SemiHeapSort.is_semi_max?(2, 5, 6)).to eq false end
-            it 'reports 6 as NOT semi max in 3, 6, 7' do expect(SemiHeapSort.is_semi_max?(5, 2, 6)).to eq false end
+            it 'reports 7 as semi max in 3, 6, 7' do expect(SemiHeapSort.is_semi_max?(6, 2, 5, &@comp)).to eq true end
+            it 'reports 3 as NOT semi max in 3, 6, 7' do expect(SemiHeapSort.is_semi_max?(2, 5, 6, &@comp)).to eq false end
+            it 'reports 6 as NOT semi max in 3, 6, 7' do expect(SemiHeapSort.is_semi_max?(5, 2, 6, &@comp)).to eq false end
 
-            it 'reports 4 as semi max in 4, 8' do expect(SemiHeapSort.is_semi_max?(4, 8, 9)).to eq true end
-            it 'reports 8 as NOT semi max in 4, 8' do expect(SemiHeapSort.is_semi_max?(8, 4, 9)).to eq false end
+            it 'reports 4 as semi max in 4, 8' do expect(SemiHeapSort.is_semi_max?(3, 7, nil, &@comp)).to eq true end
+            it 'reports 8 as NOT semi max in 4, 8' do expect(SemiHeapSort.is_semi_max?(7, 3, nil, &@comp)).to eq false end
 
-            it 'reports 2 as semi max in 2, 4, 5' do expect(SemiHeapSort.is_semi_max?(1, 3, 4)).to eq true end
-            it 'reports 4 as semi max in 2, 4, 5' do expect(SemiHeapSort.is_semi_max?(3, 1, 4)).to eq true end
-            it 'reports 5 as semi max in 2, 4, 5' do expect(SemiHeapSort.is_semi_max?(4, 1, 3)).to eq true end
+            it 'reports 2 as semi max in 2, 4, 5' do expect(SemiHeapSort.is_semi_max?(1, 3, 4, &@comp)).to eq true end
+            it 'reports 4 as semi max in 2, 4, 5' do expect(SemiHeapSort.is_semi_max?(3, 1, 4, &@comp)).to eq true end
+            it 'reports 5 as semi max in 2, 4, 5' do expect(SemiHeapSort.is_semi_max?(4, 1, 3, &@comp)).to eq true end
         end
+
+
+
 
         describe 'semi heap sort' do
             it 'sorts items correctly' do
-                expect(SemiHeapSort.semi_heap_sort((0..7).to_a)).to eq [0, 6, 2, 1, 3, 4, 7, 5]
+                expect(SemiHeapSort.semi_heap_sort((0..7).to_a, &@comp)).to eq [0, 6, 2, 1, 3, 4, 7, 5]
             end
         end
     end
