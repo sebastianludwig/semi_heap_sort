8abd27839adcb354a00e10257cbb15b172eccb6d
Sebastian Ludwig <sebastian@lurado.de>
2015-06-10 01:20:28 +0200

    Implemented Figure 2 (a) specs which lead to a full stack JS port.

--- spec/javascripts/semi_heap_spec.js
+++ spec/javascripts/semi_heap_spec.js
@@ -1,5 +1,82 @@
 describe("SemiHeapSort", function() {
-  it("cleans the number by removing spaces and dashes", function() {
-    expect(CreditCard.cleanNumber("123 4-5")).toEqual("12345");
-  });
+    var tournament = [[]];
+    var comp = function(a, b) { 
+        return tournament[a - 1][b - 1] == 1 ? -1 : 1
+    };
+
+    describe("Figure 2 (a)", function() {
+
+        beforeEach(function() {
+            var n = null;
+            tournament = [
+                [n, 1, 1],
+                [0, n, 1],
+                [0, 0, n]
+            ];
+        });
+
+        describe("max", function() {
+            it("finds 1 as max", function() {
+                expect(SemiHeapSort.max(1, 2, 3, comp)).toEqual(1);
+            });
+
+            it("finds 1 as max regardless of the argument order", function() {
+                expect(SemiHeapSort.max(1, 3, 2, comp)).toEqual(1);
+                expect(SemiHeapSort.max(2, 1, 3, comp)).toEqual(1);
+                expect(SemiHeapSort.max(3, 1, 2, comp)).toEqual(1);
+                expect(SemiHeapSort.max(2, 3, 1, comp)).toEqual(1);
+                expect(SemiHeapSort.max(3, 2, 1, comp)).toEqual(1);
+            });
+
+            it("reports 1 as max", function() {
+                expect(SemiHeapSort.is_max(1, 2, 3, comp)).toBe(true);
+            });
+            it("reports 2 as NOT max", function() {
+                expect(SemiHeapSort.is_max(2, 1, 3, comp)).toBe(false);
+            });
+            it("reports 3 as NOT max", function() {
+                expect(SemiHeapSort.is_max(3, 1, 2, comp)).toBe(false);
+            });
+        });
+
+        describe("is_semi_max", function() {
+            it("reports 1 as semi max", function() { 
+                expect(SemiHeapSort.is_semi_max(1, 2, 3, comp)).toBe(true);
+            });
+            it("reports 2 as NOT semi max", function() { 
+                expect(SemiHeapSort.is_semi_max(2, 1, 3, comp)).toBe(false);
+            });
+            it("reports 3 as NOT semi max", function() { 
+                expect(SemiHeapSort.is_semi_max(3, 1, 2, comp)).toBe(false);
+            });
+        });
+
+        describe("semi_heap_sort", function() {
+            it("sorts items correctly", function() {
+                expect(SemiHeapSort.sort([1, 2, 3], comp)).toEqual([1, 2, 3]);
+            });
+        });
+    });
+
+
+    describe("Figure 2 (d)", function() {
+
+        beforeEach(function() {
+            var n = null
+            tournament = [
+                [n, 0, 1],
+                [1, n, 0],
+                [0, 1, n]
+            ];
+        });
+
+        describe("beats", function() {
+            it("works", function() {
+                expect(SemiHeapSort.beats(1, 3, comp)).toBe(true);
+                expect(SemiHeapSort.beats(2, 1, comp)).toBe(true);
+                expect(SemiHeapSort.beats(3, 2, comp)).toBe(true);
+            });
+        });
+    });
+
 });
--- src/semi_heap_sort.js
+++ src/semi_heap_sort.js
@@ -1,5 +1,92 @@
-var CreditCard = {
-  cleanNumber: function(number) {
-    return number.replace(/[- ]/, "");
-  }
+var SemiHeapSort = {
+    beats: function(a_i, a_j, comparator) {
+        if (a_i == null) {
+            return false;
+        }
+        if (a_j == null) {
+            return true;
+        }
+        var result = comparator(a_i, a_j);
+        if (result != 1 & result != -1) {
+            throw "invalid comparison result " + result + " - must be -1 or 1";
+        }
+        return result == -1;
+    },
+
+    max: function(a_i, a_j, a_k, comparator) {
+        if (this.beats(a_i, a_j, comparator) && this.beats(a_i, a_k, comparator)) {
+            return a_i;
+        }
+        if (this.beats(a_j, a_i, comparator) && this.beats(a_j, a_k, comparator)) {
+            return a_j;
+        }
+        if (this.beats(a_k, a_i, comparator) && this.beats(a_k, a_j, comparator)) {
+            return a_k;
+        }
+        return null;
+    },
+
+    is_max: function(a_i, a_j, a_k, comparator) {
+        return this.max(a_i, a_j, a_k, comparator) == a_i;
+    },
+
+    is_semi_max: function(a_i, a_j, a_k, comparator) {
+        return !this.is_max(a_j, a_i, a_k, comparator) && !this.is_max(a_k, a_i, a_j, comparator);
+    },
+
+    left: function(index) {
+        return 2 * index + 1;
+    },
+
+    right: function(index) {
+        return 2 * index + 2;
+    },
+
+    replace: function(a, i, comparator) {
+        if (a[this.left(i)] == null && a[this.right(i)] == null) {
+            a[i] = null;
+        } else if (this.beats(a[i], a[this.left(i)], comparator) && this.beats(a[this.left(i)], a[this.right(i)], comparator)) {
+            a[i] = a[this.left(i)];
+            this.replace(a, this.left(i), comparator);
+        } else {
+            a[i] = a[this.right(i)];
+            this.replace(a, this.right(i), comparator);
+        }
+    },
+
+    semi_heapify: function(a, i, comparator) {
+        if (!this.is_semi_max(a[i], a[this.left(i)], a[this.right(i)], comparator)) {
+            var winner = this.max(a[i], a[this.left(i)], a[this.right(i)], comparator);
+            if (winner == null) {
+                throw "no winner";
+            }
+            var winner_index = winner == a[this.left(i)] ? this.left(i) : this.right(i);
+            var temp = a[i];
+            a[i] = a[winner_index];
+            a[winner_index] = temp;
+            this.semi_heapify(a, winner_index, comparator);
+        }
+    },
+
+    build_semi_heap: function(a, comparator) {
+        a = a.concat(new Array(a.length).fill(null));
+
+        for (i = Math.floor((a.length - 1) / 4); i <= 0; i--) { 
+           self.semi_heapify(a, i, comparator);
+        }
+
+        return a;
+    },
+
+    sort: function(elements, comparator) {
+        var a = this.build_semi_heap(elements, comparator);
+
+        var result = [];
+        while (a[this.left(0)] != null || a[this.right(0)] != null) {
+            result.push(a[0]);
+            this.replace(a, 0, comparator);
+        }
+        result.push(a[0]);
+        return result;
+    }
 }
